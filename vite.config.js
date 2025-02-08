abd749f304dcdc1e32c4f3316f07b8c3b9c4339c
Ochife Ogechukwu <ochifeoge@gmail.com>
2025-02-08 13:39:49 +0100

    set base property in defineConfig for Vite project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
+  base: "/t-lexa-project/",
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
